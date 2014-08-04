af4689f6caa7a7b00ef86ca2e588376ed3481cb9
Fen <petersm@live.co.uk>
2014-08-04 03:09:24 +0100

    Consolodated functions within demo.html, general tidy up of script

--- testobject.js
+++ testobject.js
@@ -1,3 +1,3 @@
-var spinningCube = {width:10, height:10, spin:true. pulse:false};
+var spinningCube = {width:10, height:10, spin:true, pulse:false, color:"red"};
 
-var pulsingCube = {width:5, height:5, spin:0, pulse:true}
+var pulsingCube = {width:5, height:5, spin:0, pulse:true, colour:"blue"};
