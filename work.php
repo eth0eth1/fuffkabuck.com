daf8ea4fece4dd2d84cbc042e121dacf939ef5be
Elly <9212227@gmail.com>
2014-09-10 23:09:14 +0100

    Replaced work.html with partial work.php

---
+++ work.php
@@ -0,0 +1,9 @@
+<article>
+	<div class="content">
+		<p><center>Page under construction. Check back soon.</center></p>
+		<img class="Banner" src="img/ConstructionBanner.png">
+
+	</div>
+</article>
+</body>
+</html>
