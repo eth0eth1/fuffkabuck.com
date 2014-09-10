ecc11f550789555dd0d85ca0dfabded1900b2c18
Fen <petersm@live.co.uk>
2014-09-10 23:39:03 +0100

    Converted Index, Demo and About to PHP. Included PHP header on pages.

---
+++ demo.php
@@ -0,0 +1,21 @@
+<?php require "header.php"; ?>
+<article>
+    <div class="content">
+
+        
+        <p><center>Below are all available demos to view.</center> </p>
+
+        <a href="weathergrid.html">
+        <div align="center" class="previewButton" style="background-image: url(img/testButton.jpg);cursor:hand;"
+        >
+
+            <!--<img src="img/testButton.jpg"> -->
+            <h2>Weather Based Pixel Grid</h2>
+         </div>
+         </a>
+
+
+    </div>
+</article>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php require "header.php"; ?>
+<article>
+  <div class="content">
+   <img class="splash" src="img/splash1.png">
+   <img class="splash" src="img/splash2.png">
+   <img class="splash" src="img/splash3.png">
+   <p>I am a programmer slash artist and this is my demo website. Feel free to look around - new and possibly exciting projects will be uploaded as and when compelted.</p>
+
+    <p>Lorem ipsums nom nom dolor sit amet, consectetur adipiscing elit. Etiam varius vel tortor a iaculis. Nullam egestas gravida risus, sit amet dictum dolor facilisis condimentum. Morbi id ligula vestibulum elit facilisis tempus id non lorem. Proin commodo lectus et accumsan lacinia. In elementum pretium felis a aliquet. Fusce eu congue risus. Donec sit amet luctus ante. Curabitur semper lectus non consequat rutrum. Suspendisse potenti. Donec volutpat lectus vitae velit tristique semper. Pellentesque lobortis cursus neque eget semper.</p>
+
+   <p> Here's some more text that's not really about anything at all.</p>
+  </div>
+</article>
+</body>
+</html>
