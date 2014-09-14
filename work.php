95a02c7cd5999afba4856b6812d535981a8d1893
Fen <petersm@live.co.uk>
2014-09-14 15:33:46 +0100

    Updated headers on Work page.

--- work.php
+++ work.php
@@ -3,9 +3,22 @@
 ?>
 <article>
 	<div class="content">
+
+
 		<p><center>Page under construction. Check back soon.</center></p>
 		<img class="Banner" src="img/ConstructionBanner.png">
-		
+
+		<div> <!--3D Work-->
+
+		<h2 class="previewText">3D Work</h2>
+
+		</div>
+
+		<div> <!--Graphic WOrk-->
+		<h2 class="previewText">Graphic Work</h2>
+
+		</div>
+
 
 	</div>
 </article>
