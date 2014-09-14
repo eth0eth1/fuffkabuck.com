d2ced8328a4d603ae51e9f3d3633189bfeb1c4a9
Fen <petersm@live.co.uk>
2014-09-14 15:44:10 +0100

    Updated headers on Work page.

--- work.php
+++ work.php
@@ -13,17 +13,23 @@
 			<h2>3D Work</h2> <!--Section head-->
 
 			<!--Preview Buttons | Placeholder images so far-->
+			<a href="weathergrid.html">
 			<div align="center" class="previewButton" stlye="background-image: url(img/testButton.jpg);cursor:hand;"
 			>
 			</div>
+			</a>
 
+			<a href="weathergrid.html">
 			<div align="center" class="previewButton" stlye="background-image: url(img/testButton.jpg);cursor:hand;"
 			>
 			</div>
+			</a>
 
+			<a href="weathergrid.html">
 			<div align="center" class="previewButton" stlye="background-image: url(img/testButton.jpg);cursor:hand;"
 			>
 			</div>
+			</a>
 
 
 
