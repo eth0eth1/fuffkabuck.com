aa191eeaf00039bf5a208adf47a96e47e2b4daed
Fen <petersm@live.co.uk>
2014-09-15 23:11:05 +0100

    Updated H2 class to display inline

--- work.php
+++ work.php
@@ -11,19 +11,19 @@
 
 			<!--Preview Buttons | Placeholder images so far-->
 
-			<div class="previewButton;" style="background-image: url(img/testButton.jpg);cursor:hand;">
+			<div class="previewButton" style="background-image: url(img/testButton.jpg);cursor:hand;">
 
 				<h2>Test Text</h2>
 
 			</div>
 
-			<div class="previewButton;" style="background-image: url(img/testButton.jpg);cursor:hand;">
+			<div class="previewButton" style="background-image: url(img/testButton.jpg);cursor:hand;">
 
 				<h2>Test Text</h2>
 
 			</div>
 
-			<div class="previewButton;" style="background-image: url(img/testButton.jpg);cursor:hand;">
+			<div class="previewButton" style="background-image: url(img/testButton.jpg);cursor:hand;">
 
 				<h2>Test Text</h2>
 
