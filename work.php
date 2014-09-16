dc3d4928f88abe4fd94562c61e1e4980ef36f752
Elly <9212227@gmail.com>
2014-09-16 20:52:40 +0100

    Remove wrapped divs on work page

--- work.php
+++ work.php
@@ -1,41 +1,26 @@
 <?php
   require "header.php";
 ?>
+
 <article>
 	<div class="content">
+	<div class="inPageHeader">
+		<h2>Graphic Work</h2>
+	</div>
+		<div class="previewButton"></div>
+		<div class="previewButton"></div>
+		<div class="previewButton"></div>
 
-		<div style="display:block">
-		<div class="inPageHeader"><h2>Graphic Work</h2></div>
 
-			<div class="previewButton">
-			</div>
-
-			<div class="previewButton">
-			</div>
-
-			<div class="previewButton">
-			</div>
-
-		</div>
-
-		<div style="display:block">
-
-		<div class="inPageHeader"><h2>3D Work</h2>
-
-			<div class="previewButton">
-			</div>
-
-			<div class="previewButton">
-			</div>
-
-			<div class="previewButton">
-			</div>
-
-		</div>
-		
-
+	<div class="inPageHeader">
+		<h2>3D Work</h2>
+	</div>
+		<div class="previewButton"></div>
+		<div class="previewButton"></div>
+		<div class="previewButton"></div>
 	</div>
 </article>
+
 <?php 
 require "footer.php";
 ?>
