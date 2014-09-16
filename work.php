d508b1fc56d8fbd72e52eb414e96518f8c74b453
Fen <petersm@live.co.uk>
2014-09-16 22:20:31 +0100

    Changed previewButton class to be square. Removed height and replaced with paddint-top equal to width %

--- work.php
+++ work.php
@@ -10,7 +10,7 @@
 			<div class="previewButton"></div>
 
 			<h2>3D Work</h2>
-			<div class="previewButton"><img src="url(img/ThumbFICollectionPreview.png)" alt="TF2FICollectionPreview" height="100%"></div>
+			<div class="previewButton"><img src="img/ThumbFICollectionPreview.png" alt="TF2FICollectionPreview" height="100%"></div>
 			<div class="previewButton"></div>
 			<div class="previewButton"></div>
 	</div>
