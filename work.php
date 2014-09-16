f17e95fd6cf458cf4d9647707a2fda6537f9f638
Fen <petersm@live.co.uk>
2014-09-16 19:59:16 +0100

    Updated divs on work.php

--- work.php
+++ work.php
@@ -5,10 +5,31 @@
 	<div class="content">
 
 
-	<h2 class="inPageHeaderDiv">3D Work</h2>
+	<h2 class="inPageHeaderDiv">Graphic Work</h2>
+
+	<div style="display:block;">
+
+	<div class="previewButton">
+	</div>
+
+	<div class="previewButton">
+	</div>
+
+	<div class="previewButton">
+	</div>
+
+
+	
+	</div>
 
 
 	<h2 class="inPageHeaderDiv">3D Work</h2>
+
+	<div style="display:block;"> 
+
+
+
+	</div>
 		
 
 	</div>
