9e3bf1c85f39f1bc4bf017b1e780e3f5d8a04ea9
Fen <petersm@live.co.uk>
2014-09-17 18:43:13 +0100

    Updated TF2 preview image size and added Steam Workshop link

--- work.php
+++ work.php
@@ -10,7 +10,9 @@
 			<div class="previewButton"></div>
 
 			<h2>3D Work</h2>
+			<a href="http://steamcommunity.com/sharedfiles/filedetails/?id=314000062">
 			<div class="previewButton"><img class="previewImage" src="img/ThumbFICollectionPreview.png" alt="TF2FICollectionPreview"></div>
+			</a>
 			<div class="previewButton"></div>
 			<div class="previewButton"></div>
 	</div>
