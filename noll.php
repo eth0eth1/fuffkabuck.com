27bd274ef125bbb03bc89eaa7ed2c4f9dd56780f
Fen <petersm@live.co.uk>
2014-09-18 16:53:11 +0100

    ImagesLoaded was removed from Masonry and undocumented, found lib elsewhere and added to imports

--- noll.php
+++ noll.php
@@ -6,9 +6,8 @@ require "header.php";
 
 <div class="content">
 
-	
+	<iframe class="item" width="600" height="600" src="https://sketchfab.com/models/adf631c73bdf4e4db53fe0cc9c20fbb1/embed" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" onmousewheel=""></iframe>
 	<div id="container"
-  		<div class="item"><iframe class="item" width="600" height="600" src="https://sketchfab.com/models/adf631c73bdf4e4db53fe0cc9c20fbb1/embed" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" onmousewheel=""></iframe></div>
 		<div class="item"><img src="img/NollPoseWork.jpg" alt="Image showing design work from Noll project"></div>
 		<div class="item"><img src="img/TreasureNoll.jpg" alt="Image showing design work from Noll project"></div>
 		<div class="item"><img src="img/DesignNoll.jpg" alt="Image showing design work from Noll project"></div>
@@ -23,15 +22,11 @@ require "header.php";
 <script src="imagesloaded.pkgd.min.js"></script>
 
 <script>
-document.getElementById("container").innerHTML = "Paragraph changed.";
 var container = document.querySelector('#container');
-var msnry = new Masonry(container, {
-	columnWidth:150,
-	itemSelector: '.item'
-});
-imagesLoaded( container, function() {
-	document.getElementById("container").innerHTML = "Javascript function is doing stuff";
-  	msnry.layout();
+var msnry = new Masonry( container, {
+  // options
+  columnWidth: 150,
+  itemSelector: '.item'
 });
 </script>
 
