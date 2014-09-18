4e69c66d683cbdb6e08090ee4c874cffaaa284f0
Fen <petersm@live.co.uk>
2014-09-18 14:13:28 +0100

    Added masonry.js to site for content layout. Tested on noll.php

--- noll.php
+++ noll.php
@@ -9,7 +9,7 @@ require "header.php";
 	<div id="container" class="js-masonry"
 		data-masonry-options='{ "columnWidth":200, "itemselector": "item"}'>
 
-		<iframe width="640" height="480" src="https://sketchfab.com/models/adf631c73bdf4e4db53fe0cc9c20fbb1/embed" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" onmousewheel=""></iframe>
+		<iframe width="600" height="600" src="https://sketchfab.com/models/adf631c73bdf4e4db53fe0cc9c20fbb1/embed" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" onmousewheel=""></iframe>
 		<img class="item" src="img/NollPoseWork.jpg" alt="Image showing design work from Noll project">
 		<img class="item" src="img/TreasureNoll.jpg" alt="Image showing design work from Noll project">
 		<img class="item" src="img/DesignNoll.jpg" alt="Image showing design work from Noll project">
