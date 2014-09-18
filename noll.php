e3589caab575c1f10851acabf2c229bbc435338f
Fen <petersm@live.co.uk>
2014-09-18 13:40:01 +0100

    Resized and arranged images on noll.php

--- noll.php
+++ noll.php
@@ -4,10 +4,11 @@ require "header.php";
 <article>
 <div class="content">
 <iframe width="640" height="480" src="https://sketchfab.com/models/adf631c73bdf4e4db53fe0cc9c20fbb1/embed" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" onmousewheel=""></iframe>
-<img src="img/DesignNoll.jpg" alt="Image showing design work from Noll project">
-<img src="img/TechnicalNoll.jpg" alt="Image showing design work from Noll project">
 <img src="img/NollPoseWork.jpg" alt="Image showing design work from Noll project">
 <img src="img/TreasureNoll.jpg" alt="Image showing design work from Noll project">
+<img src="img/DesignNoll.jpg" alt="Image showing design work from Noll project">
+<img src="img/TechnicalNoll.jpg" alt="Image showing design work from Noll project">
+
 
 </div>
 </article>
