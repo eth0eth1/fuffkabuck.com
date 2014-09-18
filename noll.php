0e7df15db08c1a6b8907e68a9b4a3b6bdbc6eda0
Fen <petersm@live.co.uk>
2014-09-18 15:07:42 +0100

    Reverted change to call to msnry through HTML

--- noll.php
+++ noll.php
@@ -2,8 +2,7 @@
 require "header.php";
 ?>
 
-<script src="masonry.pkgd.min.js"> //Masonry Initilisation. Waits for images to load before setting #Container as the msnry instance
-
+<script src="masonry.pkgd.min.js">
 </script>
 
 <article>
@@ -11,12 +10,13 @@ require "header.php";
 <div class="content">
 
 	<iframe class="item" width="600" height="600" src="https://sketchfab.com/models/adf631c73bdf4e4db53fe0cc9c20fbb1/embed" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" onmousewheel=""></iframe>
+	
 	<div id="container" class="js.masonry"
 		data-masonry=options='{ "columnWidth":450, "itemSelector": ".item"}'>
-		<img class="item" src="img/NollPoseWork.jpg" alt="Image showing design work from Noll project">
-		<img class="item" src="img/TreasureNoll.jpg" alt="Image showing design work from Noll project">
-		<img class="item" src="img/DesignNoll.jpg" alt="Image showing design work from Noll project">
-		<img class="item" src="img/TechnicalNoll.jpg" alt="Image showing design work from Noll project">
+		<div class="item"><img src="img/NollPoseWork.jpg" alt="Image showing design work from Noll project"></div>
+		<div class="item"><img src="img/TreasureNoll.jpg" alt="Image showing design work from Noll project"></div>
+		<div class="item"><img src="img/DesignNoll.jpg" alt="Image showing design work from Noll project"></div>
+		<div class="item"><img src="img/TechnicalNoll.jpg" alt="Image showing design work from Noll project"></div>
 
 	</div>
 
