cf743bc8ea5d49093fa7ad73dc16cfe02e4a55de
Fen <petersm@live.co.uk>
2014-09-18 15:37:49 +0100

    Changed msnry to JS, again

--- noll.php
+++ noll.php
@@ -10,8 +10,7 @@ require "header.php";
 <div class="content">
 
 	
-	<div id="container" class="js-masonry"
-  		data-masonry-options='{ "columnWidth": 150, "itemSelector": ".item" }'>
+	<div id="container"
   		<div class="item"><iframe class="item" width="600" height="600" src="https://sketchfab.com/models/adf631c73bdf4e4db53fe0cc9c20fbb1/embed" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" onmousewheel=""></iframe></div>
 		<div class="item"><img src="img/NollPoseWork.jpg" alt="Image showing design work from Noll project"></div>
 		<div class="item"><img src="img/TreasureNoll.jpg" alt="Image showing design work from Noll project"></div>
@@ -22,7 +21,10 @@ require "header.php";
 
 <script>
 var container = document.quertSelector('#container');
-var msnry = new Masonry(container);
+var msnry = new Masonry(container, {
+	columnWidth:150,
+	itemSelector: '.item'
+});
 imagesLoaded( container, function() {
   msnry.layout();
 </script>
