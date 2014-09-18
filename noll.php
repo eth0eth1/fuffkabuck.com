d4c925a31a9fe108768dbd7018dbb3fb8b184503
Fen <petersm@live.co.uk>
2014-09-18 14:10:45 +0100

    Added masonry.js to site for content layout. Tested on noll.php

--- noll.php
+++ noll.php
@@ -6,14 +6,14 @@ require "header.php";
 <article>
 <div class="content">
 
-	<div class="js-masonry"
+	<div id="container" class="js-masonry"
 		data-masonry-options='{ "columnWidth":200, "itemselector": "item"}'>
 
-		<div><iframe width="640" height="480" src="https://sketchfab.com/models/adf631c73bdf4e4db53fe0cc9c20fbb1/embed" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" onmousewheel=""></iframe></div>
-		<div><img src="img/NollPoseWork.jpg" alt="Image showing design work from Noll project"></div>
-		<div><img src="img/TreasureNoll.jpg" alt="Image showing design work from Noll project"></div>
-		<div><img class="item" src="img/DesignNoll.jpg" alt="Image showing design work from Noll project"></div>
-		<div><img class="item" src="img/TechnicalNoll.jpg" alt="Image showing design work from Noll project"></div>
+		<iframe width="640" height="480" src="https://sketchfab.com/models/adf631c73bdf4e4db53fe0cc9c20fbb1/embed" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" onmousewheel=""></iframe>
+		<img class="item" src="img/NollPoseWork.jpg" alt="Image showing design work from Noll project">
+		<img class="item" src="img/TreasureNoll.jpg" alt="Image showing design work from Noll project">
+		<img class="item" src="img/DesignNoll.jpg" alt="Image showing design work from Noll project">
+		<img class="item" src="img/TechnicalNoll.jpg" alt="Image showing design work from Noll project">
 
 </div>
 
