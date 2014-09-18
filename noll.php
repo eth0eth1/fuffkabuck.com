2e813890392f3297f6b95630c66e5e0600878636
Fen <petersm@live.co.uk>
2014-09-18 15:31:02 +0100

    Reverted change to call to msnry through HTML

--- noll.php
+++ noll.php
@@ -9,10 +9,10 @@ require "header.php";
 
 <div class="content">
 
-	<iframe class="item" width="600" height="600" src="https://sketchfab.com/models/adf631c73bdf4e4db53fe0cc9c20fbb1/embed" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" onmousewheel=""></iframe>
 	
 	<div id="container" class="js-masonry"
   		data-masonry-options='{ "columnWidth": 200, "itemSelector": ".item" }'>
+  		<div class="item"><iframe class="item" width="600" height="600" src="https://sketchfab.com/models/adf631c73bdf4e4db53fe0cc9c20fbb1/embed" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" onmousewheel=""></iframe></div>
 		<div class="item"><img src="img/NollPoseWork.jpg" alt="Image showing design work from Noll project"></div>
 		<div class="item"><img src="img/TreasureNoll.jpg" alt="Image showing design work from Noll project"></div>
 		<div class="item"><img src="img/DesignNoll.jpg" alt="Image showing design work from Noll project"></div>
@@ -20,7 +20,11 @@ require "header.php";
 
 	</div>
 
-
+<script>
+var container = document.quertSelector('#container');
+imagesLoaded( container, function() {
+  container.layout();
+</script>
 
 </div>
 </article>
