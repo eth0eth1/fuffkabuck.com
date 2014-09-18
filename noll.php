0198f3c0fc61b5e023748eb3b5e34c3690726b0c
Fen <petersm@live.co.uk>
2014-09-18 14:38:32 +0100

    Changed Masonry to be loaded via JS, fied overlapping images issue by waiting for images to load before initilising

--- noll.php
+++ noll.php
@@ -4,7 +4,7 @@ var container = document.querySelector('container');
 var msnry;
 // initialize Masonry after all images have loaded
 imagesLoaded( container, function() {
-  msnry = new Masonry( container, {columnWidth:450, itemSelector: '.item'} );
+  msnry = new Masonry( container );
 });
 </script>
 
