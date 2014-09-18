309fe80b484d968994814c22c37f5b40f5e1544b
Fen <petersm@live.co.uk>
2014-09-18 02:04:44 +0100

    Updated Noll.php page. Updated Thumb for Noll.php on work page

---
+++ noll.php
@@ -0,0 +1,13 @@
+<?php
+require "header.php";
+?>
+<article>
+<iframe width="640" height="480" src="https://sketchfab.com/models/adf631c73bdf4e4db53fe0cc9c20fbb1/embed" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" onmousewheel=""></iframe>
+
+<p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;">
+ 
+</p>
+</article>
+<?php
+require "footer.php";
+?>
