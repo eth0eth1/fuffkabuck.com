4a3ebd24de391aa1e0dca22eb777a29f78ca36eb
Fen <petersm@live.co.uk>
2014-10-18 13:24:37 +0100

    Debug

--- weathergrid.php
+++ weathergrid.php
@@ -19,7 +19,12 @@ css?family=Open+Sans:400,300" />
 <?php
 $user_ip = getenv('REMOTE_ADDR');
 $geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$user_ip"));
-
+$city = $geo["geoplugin_city"];
+$region = $geo["geoplugin_regionName"];
+$country = $geo["geoplugin_countryName"];
+echo "City: ".$city."<br>";
+echo "Region: ".$region."<br>";
+echo "Country: ".$country."<br>";
 /*
 geoplugin_request
 geoplugin_status
