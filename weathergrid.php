8567d7800aa18a8de22cc2fc5015aded42ffd78b
Fen <petersm@live.co.uk>
2014-10-18 13:23:30 +0100

    Debug

--- weathergrid.php
+++ weathergrid.php
@@ -16,7 +16,31 @@ css?family=Open+Sans:400,300" />
                 Your browser does not support this HTML5 tag, that sucks!
     </canvas>
 
+<?php
+$user_ip = getenv('REMOTE_ADDR');
+$geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$user_ip"));
 
+/*
+geoplugin_request
+geoplugin_status
+geoplugin_credit
+geoplugin_city
+geoplugin_region
+geoplugin_areaCode
+geoplugin_dmaCode
+geoplugin_countryCode
+geoplugin_countryName
+geoplugin_continentCode
+geoplugin_latitude
+geoplugin_longitude
+geoplugin_regionCode
+geoplugin_regionName
+geoplugin_currencyCode
+geoplugin_currencySymbol
+geoplugin_currencySymbol_UTF8
+geoplugin_currencyConverter
+*/
+?>
 
 <script>
 var ctx = document.getElementById("weatherGrid").getContext("2d");
