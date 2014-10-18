09ca67681afca7eb2b8305650adb5bb1605ee06a
Fen <petersm@live.co.uk>
2014-10-18 15:03:29 +0100

    Updated Noll.PHP to try and fix Masonry issue

--- noll.php
+++ noll.php
@@ -8,7 +8,7 @@ require "header.php";
 
 	<div id="container" class="js-masonry"
   		data-masonry-options='{ "columnWidth": 150, "itemSelector": ".item" }'>
-  		<div class="item"><iframe class="item" width="600" height="600" src="https://sketchfab.com/models/adf631c73bdf4e4db53fe0cc9c20fbb1/embed" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" onmousewheel=""></iframe></div>
+  		<!--<div class="item"><iframe class="item" width="600" height="600" src="https://sketchfab.com/models/adf631c73bdf4e4db53fe0cc9c20fbb1/embed" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" onmousewheel=""></iframe></div>-->
 		<div class="item"><img src="img/NollPoseWork.jpg" alt="Image showing design work from Noll project"></div>
 		<div class="item"><img src="img/TreasureNoll.jpg" alt="Image showing design work from Noll project"></div>
 		<div class="item"><img src="img/DesignNoll.jpg" alt="Image showing design work from Noll project"></div>
