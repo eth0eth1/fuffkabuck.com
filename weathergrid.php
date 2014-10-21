40a667988f560148cc4d4a6291770a40ad6ee1e5
Fen <petersm@live.co.uk>
2014-10-21 20:18:25 +0100

    Removed Debug alert from getWeather function in weathergrid

--- weathergrid.php
+++ weathergrid.php
@@ -139,7 +139,6 @@ function getColor(str){
 
 function getWeather(loc){
     
-    alert(loc);
     if(typeof weather === 'undefined'){
 
         console.log("Getting weather from the internet...");
